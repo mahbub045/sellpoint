e56bf879f5bc276d1f331804f09ea54e047728a6
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2023-10-01 00:47:05 +0600

    add secret key

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,7 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 export default NextAuth({
+    secret: process.env.SECRET,
     session: {
         strategy: 'jwt',
     },
