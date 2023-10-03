3251d4cb2b3a546a6e3e422281ebde7a86d64353
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2023-10-03 21:38:54 +0600

    change preloader className

--- components/Preloader.jsx
+++ components/Preloader.jsx
@@ -1,13 +1,13 @@
 const Preloader = () => {
     return (
         <div className="fixed top-0 left-0 w-full h-full bg-transparent flex justify-center items-center">
-            <div className="wrapper">
-                <div className="circle bg-black dark:bg-white"></div>
-                <div className="circle bg-black dark:bg-white"></div>
-                <div className="circle bg-black dark:bg-white"></div>
-                <div className="shadow bg-black dark:bg-white"></div>
-                <div className="shadow bg-black dark:bg-white"></div>
-                <div className="shadow bg-black dark:bg-white"></div>
+            <div className="preloaderWrapper">
+                <div className="preloaderCircle bg-black dark:bg-white"></div>
+                <div className="preloaderCircle bg-black dark:bg-white"></div>
+                <div className="preloaderCircle bg-black dark:bg-white"></div>
+                <div className="preloaderShadow bg-black dark:bg-white"></div>
+                <div className="preloaderShadow bg-black dark:bg-white"></div>
+                <div className="preloaderShadow bg-black dark:bg-white"></div>
             </div>
         </div>
     );
