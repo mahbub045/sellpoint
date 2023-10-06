b04cffc78e861bd18ffe8c32f401b8753b45e7af
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2023-10-06 23:57:31 +0600

    add location icon before customer location

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -262,7 +262,8 @@ const ProductScreen = () => {
                         {/* Delivery Information */}
                         <div className="p-5">
                             <div className="mb-4">
-                                <h5 className='inline-block'>Delivery</h5>
+
+                                <h5 className='inline-block font-semibold'>Delivery</h5>
                                 <button className='inline-block ml-4 text-emerald-600 hover:underline'>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 inline-block">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
@@ -272,7 +273,13 @@ const ProductScreen = () => {
                                 {!userDetails ?
                                     <p className='text-slate-400 dark:text-slate-600'>No Delivery Address Found</p>
                                     :
-                                    <p>{userDetails.address}</p>
+                                    <p>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 inline-block">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                        </svg>
+                                        {userDetails.address}
+                                    </p>
                                 }
                             </div>
                             <hr />
