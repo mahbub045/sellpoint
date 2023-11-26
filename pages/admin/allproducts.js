29d1094c969d6e81f44378aae6e78c803326536f
mahbub045 <mahbubrmn45@gmail.com>
2023-11-26 19:16:54 +0600

    add pagination

--- pages/admin/allproducts.js
+++ pages/admin/allproducts.js
@@ -9,6 +9,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Fragment, useContext, useState } from "react";
+import ReactPaginate from 'react-paginate';
 
 const Allproducts = ({ productDetails, categoryDetails, searchData }) => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -50,6 +51,31 @@ const Allproducts = ({ productDetails, categoryDetails, searchData }) => {
 
         setDataToShow(filteredData);
     };
+
+    ////////for table pagination///////
+    const [currentPage, setCurrentPage] = useState(0);
+    const productsPerPage = 2; // Number of products to display per page
+
+    const indexOfLastProduct = (currentPage + 1) * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = productDetails
+        .map((item) => item.products)
+        .flat() // Flatten the array of products
+        .filter((product) =>
+            product.name.toLowerCase().includes(searchProduct.toLowerCase())
+        ); // Filter products based on search query
+
+    const paginatedProducts = currentProducts.slice(
+        indexOfFirstProduct,
+        indexOfLastProduct
+    );
+    const totalPages = Math.ceil(currentProducts.length / productsPerPage);
+
+    // Function to handle page change
+    const handlePageClick = ({ selected }) => {
+        setCurrentPage(selected);
+    };
+    ////////for table pagination end///////
 
 
     return (
@@ -235,6 +261,49 @@ const Allproducts = ({ productDetails, categoryDetails, searchData }) => {
                                             </tr>
                                         </thead>
                                         <tbody>
+                                            {paginatedProducts.map((product, index) => (
+
+                                                <tr
+                                                    className="border-b transition duration-300 ease-in-out hover:bg-emerald-50 dark:hover:bg-neutral-900 dark:border-emerald-500"
+                                                    key={index}
+                                                >
+
+                                                    <td className="whitespace-nowrap px-6 py-4 font-medium">
+                                                        {index + 1}
+                                                    </td>
+                                                    <td className="whitespace-nowrap px-6 py-4 flex gap-2">
+                                                        <Image src={product.image} alt={product.name} width={50} height={50}></Image>
+                                                        {product.name}
+                                                    </td>
+                                                    <td className="whitespace-nowrap px-6 py-4">
+                                                        Price: {product.price}
+                                                        <br />
+                                                        Discount Price: {product.discountPrice}
+                                                        <br />
+                                                        Rating:{product.rating} ||
+                                                        Reviews: {product.numReviews}
+                                                    </td>
+                                                    <td className="whitespace-nowrap px-6 py-4">
+                                                        {product.countInStock}</td>
+                                                    <td className="whitespace-nowrap px-6 py-4 ">
+                                                        <a href="" className='dark:text-white' title='Edit'>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 hover:text-emerald-600 transition inline-block mx-1">
+                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                            </svg>
+                                                        </a>
+                                                        <button
+                                                            title='Delete'
+                                                        >
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 hover:text-red-600 transition inline-block mx-1">
+                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                            </svg>
+                                                        </button>
+                                                    </td>
+                                                </tr>
+
+                                            ))}
+                                        </tbody>
+                                        {/* <tbody>
                                             {productDetails &&
                                                 productDetails.map((item) =>
                                                     item.products.map((product, index) => {
@@ -245,9 +314,9 @@ const Allproducts = ({ productDetails, categoryDetails, searchData }) => {
                                                                 return (
                                                                     <tr
                                                                         className="border-b transition duration-300 ease-in-out hover:bg-emerald-50 dark:hover:bg-neutral-900 dark:border-emerald-500"
-                                                                        key={`${item.id}_${index}`} // Use a unique identifier here
+                                                                        key={`${item.id}_${index}`}
                                                                     >
-                                                                        {/* Render your table data here */}
+                                                                    
                                                                         <td className="whitespace-nowrap px-6 py-4 font-medium">
                                                                             {index + 1}
                                                                         </td>
@@ -260,7 +329,7 @@ const Allproducts = ({ productDetails, categoryDetails, searchData }) => {
                                                                             <br />
                                                                             Discount Price: {product.discountPrice}
                                                                             <br />
-                                                                            Rating:{product.rating} ||
+                                                                            Rating: {product.rating} ||
                                                                             Reviews: {product.numReviews}
                                                                         </td>
                                                                         <td className="whitespace-nowrap px-6 py-4">
@@ -282,15 +351,15 @@ const Allproducts = ({ productDetails, categoryDetails, searchData }) => {
                                                                     </tr>
                                                                 );
                                                             } else {
-                                                                return null; // If the product doesn't match the search, don't render it
+                                                                return null;
                                                             }
                                                         } else {
                                                             return (
                                                                 <tr
                                                                     className="border-b transition duration-300 ease-in-out hover:bg-emerald-50 dark:hover:bg-neutral-900 dark:border-emerald-500"
-                                                                    key={`${item.id}_${index}`} // Use a unique identifier here
+                                                                    key={`${item.id}_${index}`}
                                                                 >
-                                                                    {/* Render your table data here */}
+                                                                    
                                                                     <td className="whitespace-nowrap px-6 py-4 font-medium">
                                                                         {index + 1}
                                                                     </td>
@@ -327,8 +396,39 @@ const Allproducts = ({ productDetails, categoryDetails, searchData }) => {
                                                         }
                                                     })
                                                 )}
-                                        </tbody>
+                                        </tbody> */}
                                     </table>
+                                    {/* pagination serial number */}
+                                    <ReactPaginate
+                                        previousLabel={
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                                            </svg>
+                                        }
+                                        nextLabel={
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                            </svg>
+                                        }
+                                        breakLabel={'...'}
+                                        pageCount={totalPages}
+                                        marginPagesDisplayed={2}
+                                        pageRangeDisplayed={3}
+                                        onPageChange={handlePageClick}
+                                        containerClassName={'flex justify-end my-8'}
+                                        activeClassName={'bg-emerald-500 text-white px-3 py-1 rounded-md mx-1 transition-all duration-300 hover:bg-emerald-600 cursor-pointer'}
+                                        previousClassName={'bg-gray-200 text-gray-600 hover:bg-gray-300 px-3 py-1 rounded-md mx-1 transition-all duration-300 cursor-pointer'}
+                                        nextClassName={'bg-gray-200 text-gray-600 hover:bg-gray-300 px-3 py-1 rounded-md mx-1 transition-all duration-300 cursor-pointer'}
+                                        pageLinkClassName={'px-3 py-1 rounded-md mx-1 transition-all duration-300'}
+                                        initialPage={currentPage}
+                                        disableInitialCallback
+                                        pageClassName={({ selected }) =>
+                                            selected
+                                                ? 'bg-emerald-500 px-3 py-1 rounded-md mx-1 transition-all duration-300 hover:bg-emerald-600 cursor-pointer'
+                                                : 'bg-gray-200  hover:bg-gray-300 px-3 py-1 rounded-md mx-1 transition-all duration-300 cursor-pointer'
+                                        }
+                                    />
+                                    {/* pagination serial number end */}
                                 </div>
                             </div>
                         </div>
