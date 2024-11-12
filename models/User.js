7f0eb8711e54aaeea667aba1671096eb7bfa0994
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-12 12:24:48 +0600

    create user api problem

--- models/User.js
+++ models/User.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-    {
-        name: { type: String, required: true },
-        phone: { type: String, required: true, unique: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        address: { type: String, required: true },
-        isAdmin: { type: Boolean, required: true, default: false },
-    },
-    {
-        timestamps: true,
-    }
+  {
+    name: { type: String, required: true },
+    phone: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    address: { type: String },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const User = mongoose.models.User || mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
