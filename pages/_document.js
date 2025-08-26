66069f50657f2511cc0bff350c1ef53c60e5879c
mahbub045 <mrahman172014@bscse.uiu.ac.bd>
2025-08-26 22:53:20 +0600

    Install PWA

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,25 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className='scroll-smooth'>
-      <Head />
+    <Html lang="en" className="scroll-smooth">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="icon"
+          href="/manifest-icon-192.maskable.png"
+          sizes="192x192"
+        />
+        <link
+          rel="icon"
+          href="/manifest-icon-512.maskable.png"
+          sizes="512x512"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
